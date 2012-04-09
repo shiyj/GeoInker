d430960021e17eb0c1bf76971f8d1165e7c1cfac
shiyj <shiyj.cn@gmail.com>
2012-04-09 23:51:29 +0800

    open database by directory

--- assets/www/app/controllers/Map.js
+++ assets/www/app/controllers/Map.js
@@ -3,39 +3,15 @@ app.controllers.map = new Ext.Controller({
 		 // create a vector layer for drawing
 	    var vector = new OpenLayers.Layer.Vector('Vector Layer', {
 	        styleMap: new OpenLayers.StyleMap({'default':{
-	            strokeColor: "#00FF00",
+	            strokeColor: "#0000FF",
 	            strokeOpacity: 1,
 	            strokeWidth: 3,
-	            fillColor: "#FF5500",
+	            fillColor: "#5500FF",
 	            fillOpacity: 0.5,
 	            pointRadius: 6,
 	            pointerEvents: "visiblePainted",
-	            label : " ${name} ",       
-	            fontColor: "red",
-	            fontSize: "12px",
-	            fontFamily: "Courier New, monospace",
-	            fontWeight: "bold",
-	            labelAlign: "cm",
-	            labelXOffset: "${xOffset}",
-	            labelYOffset: "${yOffset}"
 	        }})
 	    });
-	    var Feature = OpenLayers.Feature.Vector;
-	    var Geometry = OpenLayers.Geometry;
-	    SQLQuery.getColumns("getColumns",function(r){showSQL(r)},function(e){log(e)});
-	    function showSQL(r){
-	    	var len = r.points.length;
-	    	var features = [];
-	    	for(var i =0;i<len;i++){
-	    		var tmp =new Feature(Geometry.fromWKT(r.points[i].xypoint));
-	    		tmp.attributes = {
-	                    name: r.points[i].name
-	                };
-		    	features.push( tmp )
-	    	}
-	    	vector.addFeatures(features);
-	    }
-	    
 	    // OpenLayers' EditingToolbar internally creates a Navigation control, we
 	    // want a TouchNavigation control here so we create our own editing toolbar
 	    var toolbar = new OpenLayers.Control.Panel({
@@ -90,5 +66,45 @@ app.controllers.map = new Ext.Controller({
 	},
 	openDatabase: function(options){
 		console.log(options.filename);
+		var dbvector = new OpenLayers.Layer.Vector('Vector Layer', {
+			styleMap: new OpenLayers.StyleMap({'default':{
+				strokeColor: "#00FF00",
+				strokeOpacity: 1,
+		    	strokeWidth: 3,
+		    	fillColor: "#FF5500",
+		    	fillOpacity: 0.5,
+		    	pointRadius: 6,
+		    	pointerEvents: "visiblePainted",
+		    	label : " ${name} ",       
+		    	fontColor: "red",
+		    	fontSize: "12px",
+		    	fontFamily: "Courier New, monospace",
+		    	fontWeight: "bold",
+		    	labelAlign: "cm",
+		    	labelXOffset: "${xOffset}",
+		    	labelYOffset: "${yOffset}"
+			}})
+		});
+		var Feature = OpenLayers.Feature.Vector;
+		var Geometry = OpenLayers.Geometry;
+		var  dir = '/'+app.stores.dirList.join('/') +'/' + options.filename;
+		SQLQuery.getColumns(dir,function(r){showSQL(r)},function(e){log(e)});
+		function showSQL(r){
+			var len = r.points.length;
+			var features = [];
+			for(var i =0;i<len;i++){
+		    	var tmp =new Feature(Geometry.fromWKT(r.points[i].xypoint));
+		    	tmp.attributes = {
+		                    name: r.points[i].name
+		        	};
+				features.push( tmp )
+		    }
+			dbvector.addFeatures(features);
+			map.addLayer(dbvector)
+			app.views.viewport.setActiveItem(app.views.mapPanel);
+		} 
+	},
+	getTables: function(filename){
+		
 	}
 })
