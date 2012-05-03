7509ce97b77c37d5153b7e27907079e422d86147
shiyj <shiyj.cn@gmail.com>
2012-05-03 21:37:01 +0800

    add GPS controller

--- assets/www/app/controllers/GetGPS.js
+++ assets/www/app/controllers/GetGPS.js
@@ -1,11 +1,19 @@
-function onGeoSuccess(position) {
-    var html = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                        'Longitude: '          + position.coords.longitude             + '<br />' +
-                        'Altitude: '           + position.coords.altitude              + '<br />' +
-                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                        'Heading: '            + position.coords.heading               + '<br />' +
-                        'Speed: '              + position.coords.speed                 + '<br />' +
-                        'Timestamp: '          + new Date(position.timestamp)          + '<br />';
-    alert(html);
-}
+app.controllers.gps = new Ext.Controller({
+	getGPS: function(){
+		navigator.geolocation.getCurrentPosition(this.onGeoSuccess, onError,{  maximumAge: 3000, timeout: 10000,enableHighAccuracy: true });
+	},
+	onError:function(err){
+		alert(err);
+	},
+	onGeoSuccess:function (position) {
+	    var html = 'Latitude: '           + position.coords.latitude              + '<br />' +
+	                        'Longitude: '          + position.coords.longitude             + '<br />' +
+	                        'Altitude: '           + position.coords.altitude              + '<br />' +
+	                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
+	                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+	                        'Heading: '            + position.coords.heading               + '<br />' +
+	                        'Speed: '              + position.coords.speed                 + '<br />' +
+	                        'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+	    alert(html);
+	}	
+});
