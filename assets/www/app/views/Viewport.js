b013746823e95f843139488c553924f0403b12b7
shiyj <shiyj.cn@gmail.com>
2012-06-04 19:48:12 +0800

    add setting view

--- assets/www/app/views/Viewport.js
+++ assets/www/app/views/Viewport.js
@@ -6,13 +6,15 @@ app.views.Viewport = Ext.extend(Ext.Panel, {
         //put instances of cards into app.views namespace
         Ext.apply(app.views, {
             mapPanel: new app.views.MapPanel(),
-            dirList: new app.views.DirList()
+            dirList: new app.views.DirList(),
+            setting: new app.views.SettingPanel()
         });
         //put instances of cards into viewport
         Ext.apply(this, {
             items: [
                 app.views.mapPanel,
-                app.views.dirList
+                app.views.dirList,
+                app.views.setting
             ]
         });
         app.views.Viewport.superclass.initComponent.apply(this, arguments);
