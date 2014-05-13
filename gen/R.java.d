4a13d424e92131a536b489a9393208e856fbc90b
shiyj <shiyj.cn@gmail.com>
2014-05-13 23:26:50 +0800

    use new apk file

---
+++ gen/R.java.d
@@ -0,0 +1,11 @@
+/home/engin/workspace/android/GeoInker/gen/geoinker/droidgap/R.java \
+ : /home/engin/workspace/android/GeoInker/res/layout/main.xml \
+/home/engin/workspace/android/GeoInker/res/drawable-mdpi/ic_launcher.png \
+/home/engin/workspace/android/GeoInker/res/drawable-ldpi/ic_launcher.png \
+/home/engin/workspace/android/GeoInker/res/drawable/splash.png \
+/home/engin/workspace/android/GeoInker/res/drawable/icon.png \
+/home/engin/workspace/android/GeoInker/res/values/strings.xml \
+/home/engin/workspace/android/GeoInker/res/drawable-hdpi/ic_launcher.png \
+/home/engin/workspace/android/GeoInker/res/xml/plugins.xml \
+/home/engin/workspace/android/GeoInker/res/xml/phonegap.xml \
+/home/engin/workspace/android/GeoInker/bin/AndroidManifest.xml \
